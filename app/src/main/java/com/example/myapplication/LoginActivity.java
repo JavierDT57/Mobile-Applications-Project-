1dd446df1b26321e2b59bff9a3f19b1b210cba4a
Javier <javierdolorestolentino@gmail.com>
2024-05-13 12:09:39 -0600

    se anadio funcionalidad de prueba al LoginActivity

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,15 +1,52 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.View;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
 public class LoginActivity extends AppCompatActivity {
 
+    //Login
+    EditText mTextName;
+    EditText mTextEmail;
+    EditText mTextPassword;
+    Button mButtonRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +57,20 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //  Login
+        mTextName = findViewById(R.id.textName);
+        mTextEmail = findViewById(R.id.textEmail);
+        mTextPassword = findViewById(R.id.textPassword);
+        //Boton
+        mButtonRegister = findViewById(R.id.btnRegister);
+
+        //Evento clic del boton
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this,"Boton Registrar",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
